e80630240e7c7fe9a55279d6a358021065bbd36f
Chandresh Maurya <chandresh@aleph-labs.com>
2019-10-17 16:22:58 +0800

    Issue Fixed

--- MVVM-Example-master/MVVM-Example/Services/Encoding/ParameterEncoding.swift
+++ MVVM-Example-master/MVVM-Example/Services/Encoding/ParameterEncoding.swift
@@ -21,15 +21,15 @@ public enum ParameterEncoding {
             switch self {
             case .urlEncoding:
                 guard let urlParameters = urlParameters else { return }
-                try URLParameterEncoder().encode(urlRequest: &urlRequest, with: urlParameters)
+                try URLEncoder().encode(urlRequest: &urlRequest, with: urlParameters)
             case .jsonEncoding:
                 guard let bodyParameters = bodyParameters else { return }
-                try JSONParameterEncoder().encode(urlRequest: &urlRequest, with: bodyParameters)
+                try JSONEncoder().encode(urlRequest: &urlRequest, with: bodyParameters)
             case .urlWithJsonEncoding:
                 guard let bodyParameters = bodyParameters,
                     let urlParameters = urlParameters else { return }
-                try URLParameterEncoder().encode(urlRequest: &urlRequest, with: urlParameters)
-                try JSONParameterEncoder().encode(urlRequest: &urlRequest, with: bodyParameters)
+                try URLEncoder().encode(urlRequest: &urlRequest, with: urlParameters)
+                try JSONEncoder().encode(urlRequest: &urlRequest, with: bodyParameters)
             }
         } catch {
             throw error
@@ -40,4 +40,35 @@ public enum NetworkError: String, Error {
     case parametersNil = "Parameters were nil."
     case encodingFailed = "Parameter encoding failed."
     case missingURL = "URL is nil."
+}
+public struct JSONEncoder: ParameterEncoder {
+    public func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        do {
+            let jsonAsData = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+            urlRequest.httpBody = jsonAsData
+            if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+                urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+        } catch {
+            throw NetworkError.encodingFailed
+        }
+    }
+}
+public struct URLEncoder: ParameterEncoder {
+    public func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        guard let url = urlRequest.url else { throw NetworkError.missingURL }
+        if var urlComponents = URLComponents(url: url,
+                                             resolvingAgainstBaseURL: false), !parameters.isEmpty {
+            urlComponents.queryItems = [URLQueryItem]()
+            for (key, value) in parameters {
+                let encodingString = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+                let queryItem = URLQueryItem(name: key, value: encodingString)
+                urlComponents.queryItems?.append(queryItem)
+            }
+            urlRequest.url = urlComponents.url
+        }
+        if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+            urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        }
+    }
 }
